27637d59f01dbe1b4d3cb60b4a7e05bd8ca5d8ee
Roman De Rose <romandr37@gmail.com>
2025-01-04 04:47:59 -0300

    cambio logica styled components

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -3,25 +3,25 @@ import styled from "styled-components";
 const ActionButton = styled.button<{
   backgroundColor: string;
   boxShadowColor: string;
-}>`
-  background-color: ${(props) => props.backgroundColor};
-  color: #fff;
-  text-transform: uppercase;
-  text-align: center;
-  width: 85px;
-  height: 85px;
-  font-size: 1.1rem;
-  cursor: pointer;
-  border-radius: 50%;
-  border: none;
-  box-sizing: content-box;
-  transition: box-shadow, transform 0.2s;
-  &:hover {
-    box-shadow: 0 0 15px 0px ${(props) => props.boxShadowColor};
-  }
-  &:active {
-    transform: scale(0.9);
-  }
-`;
+}>((props) => ({
+  backgroundColor: props.backgroundColor,
+  color: "#fff",
+  textTransform: "uppercase",
+  textAlign: "center",
+  width: "85px",
+  height: "85px",
+  fontSize: "1.1rem",
+  cursor: "pointer",
+  borderRadius: "50%",
+  border: "none",
+  boxSizing: "content-box",
+  transition: "box-shadow, transform 0.2s",
+  "&:hover": {
+    boxShadow: `0 0 15px 0px ${props.boxShadowColor}`,
+  },
+  "&:active": {
+    transform: "scale(0.9)",
+  },
+}));
 
 export default ActionButton;
